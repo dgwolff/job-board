29609ccb90140c4465efceb7bef9dc7f82114975
Dylan Wolff <dylanwolff@gmail.com>
2015-02-27 09:09:30 +1100

    Update routes to only allow posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
-  resources :posts, :path => ''
+  resources :posts, :only => [:index]
   
+  root 'posts#index'
+
+
+  match '*path' => redirect('/'), via: :get
 end
