c15f403ff9863dd475518110edb47c5006a5f3e3
Dylan Wolff <dylanwolff@gmail.com>
2015-02-27 17:31:37 +1100

    Update routes to redirect /posts to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
+  get '/posts', to: redirect('/')
   
   resources :posts, :only => [:index]
-  
+
   root 'posts#index'
 
-
+  
   match '*path' => redirect('/'), via: :get
 end
